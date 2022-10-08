972e36bccdbde9404b498650d7a45383d89baec3
teriot-app <115329009+teriot-app@users.noreply.github.com>
2022-10-08 10:05:15 +0800

    rebuild

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Terms from "./pages/Terms/Terms";
 import Privacy from "./pages/privacy/Privacy";
 
 function App() {
   return (
     <Router>
-      <Route path="terms">
-        <Terms />
-      </Route>
-      <Route path="privacy">
-        <Privacy />
-      </Route>
+      <Routes>
+        <Route path="terms">
+          <Terms />
+        </Route>
+        <Route path="privacy">
+          <Privacy />
+        </Route>
+      </Routes>
     </Router>
   );
 }
